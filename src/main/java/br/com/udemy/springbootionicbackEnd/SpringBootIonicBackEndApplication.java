67c28bfd31d45b83d0d26e679d096b8c4ef8bfef
gildo-cordeiro <gildo.duartec@gmail.com>
2021-07-18 22:05:35 -0300

    Projeto criado

---
+++ src/main/java/br/com/udemy/springbootionicbackEnd/SpringBootIonicBackEndApplication.java
@@ -0,0 +1,13 @@
+package br.com.udemy.springbootionicbackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIonicBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIonicBackEndApplication.class, args);
+	}
+
+}

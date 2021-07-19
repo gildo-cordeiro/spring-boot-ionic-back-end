15545c9d5d97ca8a65332f7e41df9df733b4c046
gildo-cordeiro <gildo.duartec@gmail.com>
2021-07-18 22:40:10 -0300

    testando primeira classe de dominio

--- src/main/java/br/com/udemy/springbootionicbackend/resources/CategoriaResource.java
+++ src/main/java/br/com/udemy/springbootionicbackend/resources/CategoriaResource.java
@@ -1,16 +1,26 @@
 package br.com.udemy.springbootionicbackend.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.udemy.springbootionicbackend.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
+	Categoria cat1 = new Categoria(1, "Informática");
+	Categoria cat2 = new Categoria(2, "Escritorio");
+	
+	List<Categoria> lista = List.of(cat1, cat2);
+	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest esta funcionando";
+	public List<Categoria> listar() {
+		return lista;
 	}
 	
 }
